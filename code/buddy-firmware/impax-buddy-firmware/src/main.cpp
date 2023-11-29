c23b0ab051311484f11da0b32862a8c7f40b0bca
KATTA-00 <kanishkagunawarthana@gmail.com>
2023-11-29 21:20:05 +0530

    testing with wifi with nodeMCU

--- code/buddy-firmware/impax-buddy-firmware/src/main.cpp
+++ code/buddy-firmware/impax-buddy-firmware/src/main.cpp
@@ -1,12 +1,68 @@
-#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFi.h>
+#include <ESP8266WebServer.h>
+#include <DNSServer.h>
 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+
+// void setup() {
+//   Serial.begin(9600);
+
+//   // Set WiFi to station mode and disconnect from an AP if it was previously connected
+//   WiFi.mode(WIFI_STA);
+//   WiFi.disconnect();
+//   delay(100);
+
+//   Serial.println("Setup done");
+// }
+
+// void loop() {
+//   Serial.println("scan start");
+
+//   // WiFi.scanNetworks will return the number of networks found
+//   int n = WiFi.scanNetworks();
+//   Serial.println("scan done");
+//   if (n == 0) {
+//       Serial.println("no networks found");
+//   } else {
+//     Serial.print(n);
+//     Serial.println(" networks found");
+//     for (int i = 0; i < n; ++i) {
+//       // Print SSID and RSSI for each network found
+//       Serial.print(i + 1);
+//       Serial.print(": ");
+//       Serial.print(WiFi.SSID(i));
+//       Serial.print(" (");
+//       Serial.print(WiFi.RSSI(i));
+//       Serial.print(")");
+//       Serial.println((WiFi.encryptionType(i) == AUTH_OPEN)?" ":"*");
+//       delay(10);
+//     }
+//   }
+//   Serial.println("");
+
+//   // Wait a bit before scanning again
+//   delay(5000);
+// }
+
+const char* ssid = "Dialog 4G 629";
+const char* password = "189FFF07";
+
+void setup(){
+    Serial.begin(9600);
+    delay(1000);
+
+    WiFi.mode(WIFI_STA); //Optional
+    WiFi.begin(ssid, password);
+    Serial.println("\nConnecting");
+
+    while(WiFi.status() != WL_CONNECTED){
+        Serial.print(".");
+        delay(100);
+    }
+
+    Serial.println("\nConnected to the WiFi network");
+    Serial.print("Local ESP32 IP: ");
+    Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on by making the voltage LOW
-  delay(1000);                      // Wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-  delay(2000);                      // Wait for two seconds
-}
+void loop(){}
