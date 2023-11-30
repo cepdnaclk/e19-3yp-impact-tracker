1e4287bdf2cae4fe16a1d5e549aa51981c6e6069
kaushithamsilva <kaushithamsilva@gmail.com>
2023-11-30 10:19:16 +0530

    Profile Component Created

---
+++ code/client/impax/src/components/Profile/SignUp.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./SignUp.module.scss";
+const SignUp = () => {
+  return (
+    <main>
+      <div className={styles.content}></div>
+      <div className={styles.hero}></div>
+    </main>
+  );
+};
+
+export default SignUp;
