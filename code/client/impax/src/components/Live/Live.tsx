49f66b61acd1a330df8f9712a034afd28261b763
kaushithamsilva <kaushithamsilva@gmail.com>
2023-12-01 01:50:52 +0530

    Dashboard Cards Created

--- code/client/impax/src/components/Live/Live.tsx
+++ code/client/impax/src/components/Live/Live.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styles from "./Live.module.scss";
 import { BsBroadcast } from "react-icons/bs";
+import { FaEdit } from "react-icons/fa";
+import { IoMdExit } from "react-icons/io";
+import Btn from "../Buttons/Btn";
+import MonitorCard from "./MonitorCard";
 const Live = () => {
   return (
     <main className={styles.main}>
@@ -14,7 +18,23 @@ const Live = () => {
           <h2>Practice Session at Main Ground</h2>
           <span>Session #21</span>
         </div>
-        <div className={styles.controls}></div>
+        <div className={styles.controls}>
+          <Btn Icon={FaEdit} buttonType="secondary">
+            Edit Session
+          </Btn>
+          <Btn Icon={IoMdExit}>Exit Session</Btn>
+        </div>
+      </div>
+
+      <div className={styles.monitoring}>
+        <h3>Monitoring Players</h3>
+        <div className={styles.grid}>
+          <MonitorCard />
+          <MonitorCard />
+          <MonitorCard />
+          <MonitorCard />
+          <MonitorCard />
+        </div>
       </div>
     </main>
   );
