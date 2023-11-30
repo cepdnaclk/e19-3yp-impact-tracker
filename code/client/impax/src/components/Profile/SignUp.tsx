1bb299122402b300fb72117c6aca109292c5b781
kaushithamsilva <kaushithamsilva@gmail.com>
2023-11-30 12:49:43 +0530

    Sign Up Page

--- code/client/impax/src/components/Profile/SignUp.tsx
+++ code/client/impax/src/components/Profile/SignUp.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import styles from "./SignUp.module.scss";
 const SignUp = () => {
   return (
-    <main>
+    <main className={styles.main}>
       <div className={styles.content}></div>
-      <div className={styles.hero}></div>
+      <div className={styles.hero}>
+        <img
+          className={styles.coverImg}
+          src="../../src/assets/images/cover-min.jpg"
+          alt=""
+        />
+      </div>
     </main>
   );
 };
