6a3c2abf9ea8f0ee6d8f149aded2573715223c0e
KATTA-00 <kanishkagunawarthana@gmail.com>
2023-11-30 21:11:16 +0530

    Add CORS configuration and Swagger documentation

--- code/backend/dist/index.js
+++ code/backend/dist/index.js
@@ -5,12 +5,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+const corsOptions_1 = __importDefault(require("./src/config/corsOptions"));
+const config_1 = __importDefault(require("./src/config/config"));
+const swagger_jsdoc_1 = __importDefault(require("swagger-jsdoc"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const app = (0, express_1.default)();
-const port = process.env.PORT;
+dotenv_1.default.config();
+app.use(body_parser_1.default.json());
+app.use((0, cors_1.default)(corsOptions_1.default));
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use(express_1.default.json());
+const port = process.env.PORT || 5000;
+const swaggerSpec = (0, swagger_jsdoc_1.default)(config_1.default);
 app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
+    res.send(config_1.default.definition.info);
 });
+app.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerSpec));
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
---
+++ code/backend/src/config/allowedOrigins.ts
@@ -0,0 +1,10 @@
+const allowedOrigins:string[] = [
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+  "http://localhost:3000",
+  "http://10.30.7.70:3000",
+  "http://10.40.18.143:3000",
+  "**"
+];
+
+export default allowedOrigins;
