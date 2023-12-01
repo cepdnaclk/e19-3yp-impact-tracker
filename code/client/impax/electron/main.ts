d94944970a55cc09cf0693028b886fee1228cdbe
Mansitha <iammansitha@gmail.com>
2023-12-01 11:52:48 +0530

    Window icon changed

--- code/client/impax/electron/main.ts
+++ code/client/impax/electron/main.ts
@@ -20,7 +20,7 @@ const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, 'impax.ico'),
+    icon: path.join(process.env.VITE_PUBLIC, 'favicon.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
