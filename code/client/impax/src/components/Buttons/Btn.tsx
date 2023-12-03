1922b00ff5702d0c32588e56ec759c3ecceae998
kaushithamsilva <kaushithamsilva@gmail.com>
2023-12-03 12:25:43 +0530

    Added Color Option to Button

--- code/client/impax/src/components/Buttons/Btn.tsx
+++ code/client/impax/src/components/Buttons/Btn.tsx
@@ -5,6 +5,7 @@ interface BtnProps {
   Icon: React.ElementType;
   buttonType?: "primary" | "secondary";
   onClick?: () => void;
+  bgColor?: string;
   children: string | JSX.Element;
 }
 
@@ -12,13 +13,18 @@ const Btn: React.FC<BtnProps> = ({
   Icon,
   buttonType = "primary",
   onClick = () => {},
+  bgColor = buttonType == "primary" ? "rgb(14, 61, 127)" : "#313131",
   children,
 }) => {
   const btnClass =
     buttonType == "primary" ? styles.primaryBtn : styles.secondaryBtn;
 
   return (
-    <button onClick={onClick} className={btnClass}>
+    <button
+      onClick={onClick}
+      className={btnClass}
+      style={{ backgroundColor: bgColor }}
+    >
       <Icon className={styles.icon} />
       {children}
     </button>
