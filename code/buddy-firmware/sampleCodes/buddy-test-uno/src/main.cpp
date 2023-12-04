8cddee2e6dd039bbae40783abda8a071ca7e480e
Mansitha <iammansitha@gmail.com>
2023-12-04 19:40:40 +0530

    mpu 6050 init

--- code/buddy-firmware/sampleCodes/buddy-test-uno/src/main.cpp
+++ code/buddy-firmware/sampleCodes/buddy-test-uno/src/main.cpp
@@ -1,99 +1,65 @@
+// (c) Michael Schoeffler 2017, http://www.mschoeffler.de
+
+#include "Wire.h" // This library allows you to communicate with I2C devices.
 #include <Arduino.h>
 
-/**！
- * @file getAcceleration.ino
- * @brief Get the acceleration in the three directions of xyz, the range can be ±100g or ±200g
- * @n When using SPI, chip select pin can be modified by changing the value of macro H3LIS200DL_CS
- * @copyright  Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
- * @license     The MIT License (MIT)
- * @author [fengli](li.feng@dfrobot.com)
- * @version  V1.0
- * @date  2021-01-16
- * @url https://github.com/DFRobot/DFRobot_LIS
- */
+const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
 
-#include <DFRobot_LIS.h>
+int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
+int16_t gyro_x, gyro_y, gyro_z;                            // variables for gyro raw data
+int16_t temperature;                                       // variables for temperature data
 
-// When using I2C communication, use the following program to construct an object by DFRobot_H3LIS200DL_I2C
-/*!
- * @brief Constructor
- * @param pWire I2c controller
- * @param addr  I2C address(0x18/0x19)
- */
-// DFRobot_H3LIS200DL_I2C acce(&Wire,0x18);
-DFRobot_H3LIS200DL_I2C acce;
+char tmp_str[7]; // temporary variable used in convert function
 
-// When using SPI communication, use the following program to construct an object by DFRobot_H3LIS200DL_SPI
-#if defined(ESP32) || defined(ESP8266)
-#define H3LIS200DL_CS D3
-#elif defined(__AVR__) || defined(ARDUINO_SAM_ZERO)
-#define H3LIS200DL_CS 3
-#elif (defined NRF5)
-#define H3LIS200DL_CS 2 // The pin on the development board with the corresponding silkscreen printed as P2
-#endif
-/*!
- * @brief Constructor
- * @param cs  Chip selection pinChip selection pin
- * @param spi  SPI controller
- */
-// DFRobot_H3LIS200DL_SPI acce(/*cs = */H3LIS200DL_CS);
-
-void setup(void)
-{
-
-  Serial.begin(9600);
-  // Chip initialization
-  while (!acce.begin())
-  {
-    Serial.println("Initialization failed, please check the connection and I2C address settings");
-    delay(1000);
-  }
-  // Get chip id
-  Serial.print("chip id : ");
-  Serial.println(acce.getID(), HEX);
-
-  /**
-    set range:Range(g)
-              eH3lis200dl_100g,/< ±100g>/
-              eH3lis200dl_200g,/< ±200g>/
-  */
-  acce.setRange(/*Range = */ DFRobot_LIS::eH3lis200dl_100g);
-
-  /**
-    Set data measurement rate：
-      ePowerDown_0HZ = 0,
-      eLowPower_halfHZ,
-      eLowPower_1HZ,
-      eLowPower_2HZ,
-      eLowPower_5HZ,
-      eLowPower_10HZ,
-      eNormal_50HZ,
-      eNormal_100HZ,
-      eNormal_400HZ,
-      eNormal_1000HZ,
-  */
-  acce.setAcquireRate(/*Rate = */ DFRobot_LIS::eNormal_50HZ);
-  Serial.print("Acceleration:\n");
-  delay(1000);
+char *convert_int16_to_str(int16_t i)
+{ // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
 }
 
-void loop(void)
+void setup()
 {
+  Serial.begin(9600);
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
+  Wire.write(0x6B);                 // PWR_MGMT_1 register
+  Wire.write(0);                    // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+}
+void loop()
+{
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B);                        // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
+  Wire.endTransmission(false);             // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+  Wire.requestFrom(MPU_ADDR, 7 * 2, true); // request a total of 7*2=14 registers
 
-  // Get the acceleration in the three directions of xyz
-  long ax, ay, az;
-  // The measurement range can be ±100g or ±200g set by the setRange() function
-  ax = acce.readAccX(); // Get the acceleration in the x direction
-  ay = acce.readAccY(); // Get the acceleration in the y direction
-  az = acce.readAccZ(); // Get the acceleration in the z direction
-  // acce.getAcceFromXYZ(/*accx = */ax,/*accy = */ay,/*accz = */az);//The second method to obtain acceleration in three directions
-  // Print acceleration
-  Serial.print("x: ");
-  Serial.print(ax);
-  Serial.print(" g\t  y: ");
-  Serial.print(ay);
-  Serial.print(" g\t  z: ");
-  Serial.print(az);
-  Serial.println(" g");
-  delay(300);
+  // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
+  accelerometer_x = Wire.read() << 8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+  accelerometer_y = Wire.read() << 8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+  accelerometer_z = Wire.read() << 8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+  temperature = Wire.read() << 8 | Wire.read();     // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+  gyro_x = Wire.read() << 8 | Wire.read();          // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+  gyro_y = Wire.read() << 8 | Wire.read();          // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+  gyro_z = Wire.read() << 8 | Wire.read();          // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+
+  // print out data
+  Serial.print("aX = ");
+  Serial.print(convert_int16_to_str(accelerometer_x));
+  Serial.print(" | aY = ");
+  Serial.print(convert_int16_to_str(accelerometer_y));
+  Serial.print(" | aZ = ");
+  Serial.print(convert_int16_to_str(accelerometer_z));
+  // the following equation was taken from the documentation [MPU-6000/MPU-6050 Register Map and Description, p.30]
+  Serial.print(" | tmp = ");
+  Serial.print(temperature / 340.00 + 36.53);
+  Serial.print(" | gX = ");
+  Serial.print(convert_int16_to_str(gyro_x));
+  Serial.print(" | gY = ");
+  Serial.print(convert_int16_to_str(gyro_y));
+  Serial.print(" | gZ = ");
+  Serial.print(convert_int16_to_str(gyro_z));
+  Serial.println();
+
+  // delay
+  delay(1000);
 }
