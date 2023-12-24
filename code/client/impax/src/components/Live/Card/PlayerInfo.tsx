5fcde0576a5003e47495f41ac0cf6a96634c86c3
kaushithamsilva <kaushithamsilva@gmail.com>
2023-12-25 00:57:31 +0530

    added battery to live dahsbord

--- code/client/impax/src/components/Live/Card/PlayerInfo.tsx
+++ code/client/impax/src/components/Live/Card/PlayerInfo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./PlayerInfo.module.scss";
-
+import Battery from "../../Battery/Battery";
 type playerInfo = {
   jerseyNo: number;
   name: string;
@@ -13,7 +13,9 @@ const PlayerInfo: React.FC<{ playerInfo: playerInfo }> = ({ playerInfo }) => {
       <p className={styles.jerseyNo}>{playerInfo.jerseyNo.toString()}</p>
       <div className={styles.name}>
         <p className={styles.playerName}>{playerInfo.name}</p>
-        <p className={styles.device}>Device {playerInfo.device}</p>
+        <div className={styles.device}>
+          Device {playerInfo.device} <Battery batteryLevel={60} />
+        </div>
       </div>
     </div>
   );
