b61797b01525ce8c109aa2a20a60312833329a9a
kaushithamsilva <kaushithamsilva@gmail.com>
2023-12-25 00:42:02 +0530

    Added Styling to react-select

---
+++ code/client/impax/src/components/Devices/Card/SelectPlayer/SelectPlayer.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "./SelectPlayer.module.scss";
+import Select from "react-select";
+
+const options = [
+  { value: 69, label: "69 Angelo Mathews" },
+  { value: 11, label: "11 Kumar Sangakkara" },
+  { value: 7, label: "07 Dasun Shanaka" },
+];
+const SelectPlayer = () => {
+  return (
+    <Select
+      placeholder="Select a player"
+      options={options}
+      classNames={{
+        container: () => styles.selectContainer,
+        control: (state) =>
+          `${styles.selectControl} ${
+            state.isFocused && styles.selectControlFocused
+          }`,
+        option: (state) =>
+          `${styles.selectOption} ${
+            state.isFocused && styles.selectOptionFocused
+          } ${state.isSelected && styles.selectOptionSelected} ${
+            state.isRtl && styles.selectOptionNew
+          } `,
+        placeholder: () => styles.selectPlaceholder,
+        input: (state) =>
+          ` ${styles.selectInput}
+          ${state.onFocus && styles.selectInputFocused}
+    `,
+        menu: () => styles.selectMenu,
+        valueContainer: (state) =>
+          `${styles.selectValueContainer} ${
+            state.hasValue && styles.selectValueContainerHasValue
+          }`,
+      }}
+    />
+  );
+};
+
+export default SelectPlayer;
