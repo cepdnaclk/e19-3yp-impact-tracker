4365e809bc94a61ae1702055420ff03ce013dae8
kaushithamsilva <kaushithamsilva@gmail.com>
2023-12-25 00:41:38 +0530

    Added props to the mappedDevices component

--- code/client/impax/src/components/Devices/Card/MappedDevice.tsx
+++ code/client/impax/src/components/Devices/Card/MappedDevice.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import styles from "./MappedDevice.module.scss";
 import Battery from "../../Battery/Battery";
-
-const MappedDevice = () => {
+import SelectPlayer from "./SelectPlayer/SelectPlayer";
+const MappedDevice: React.FC<{ deviceID: string; batteryLevel: number }> = ({
+  batteryLevel,
+  deviceID,
+}) => {
   return (
     <div className={styles.card}>
       <div className={styles.deviceID}>
-        Device #1234 <Battery batteryLevel={10} />
+        Device {deviceID} <Battery batteryLevel={batteryLevel} />
       </div>
       <p>
         <span>Mapped To:</span>
       </p>
+      <SelectPlayer />
     </div>
   );
 };
