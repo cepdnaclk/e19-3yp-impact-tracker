831b74babf9bade6a8825f8811200df7d44e4d00
kaushithamsilva <kaushithamsilva@gmail.com>
2023-12-28 16:42:32 +0530

    Added prop to select the button type

--- code/client/impax/src/components/Buttons/Btn.tsx
+++ code/client/impax/src/components/Buttons/Btn.tsx
@@ -3,27 +3,30 @@ import styles from "./Btn.module.scss";
 
 interface BtnProps {
   Icon: React.ElementType;
-  buttonType?: "primary" | "secondary";
+  buttonStyle?: "primary" | "secondary";
   onClick?: () => void;
   bgColor?: string;
+  type?: "button" | "submit" | "reset";
   children: string | JSX.Element;
 }
 
 const Btn: React.FC<BtnProps> = ({
   Icon,
-  buttonType = "primary",
+  buttonStyle = "primary",
   onClick = () => {},
-  bgColor = buttonType == "primary" ? "rgb(14, 61, 127)" : "#313131",
+  bgColor = buttonStyle == "primary" ? "rgb(14, 61, 127)" : "#313131",
+  type = "button",
   children,
 }) => {
   const btnClass =
-    buttonType == "primary" ? styles.primaryBtn : styles.secondaryBtn;
+    buttonStyle == "primary" ? styles.primaryBtn : styles.secondaryBtn;
 
   return (
     <button
       onClick={onClick}
       className={btnClass}
       style={{ backgroundColor: bgColor }}
+      type={type}
     >
       <Icon className={styles.icon} />
       {children}
