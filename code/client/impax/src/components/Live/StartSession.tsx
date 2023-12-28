d06b14550ddcdc8faa536950495a56c93f675ecb
kaushithamsilva <kaushithamsilva@gmail.com>
2023-12-28 16:44:48 +0530

    New Component to set the new session

---
+++ code/client/impax/src/components/Live/StartSession.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "./StartSession.module.scss";
+import Btn from "../Buttons/Btn";
+import { FaArrowRight } from "react-icons/fa";
+
+const StartSession: React.FC<{ onClick: () => void }> = ({ onClick }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <h2>Start a new session</h2>
+        <span>To track and monitor impacts, start a new session</span>
+      </div>
+      <form className={styles.sessionForm}>
+        <input
+          type="text"
+          name="session-name"
+          id="session-name"
+          placeholder="Enter Session Name"
+        />
+        <Btn
+          type="submit"
+          Icon={FaArrowRight}
+          buttonStyle="primary"
+          onClick={onClick}
+        >
+          Start Session
+        </Btn>
+      </form>
+    </div>
+  );
+};
+
+export default StartSession;
