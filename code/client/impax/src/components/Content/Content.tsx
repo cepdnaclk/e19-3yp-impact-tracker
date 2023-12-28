7824989e1246b4e5474ca07cbc54a30ea8fd5cda
kaushithamsilva <kaushithamsilva@gmail.com>
2023-12-28 16:42:50 +0530

    Added Player Management Screen to Content

--- code/client/impax/src/components/Content/Content.tsx
+++ code/client/impax/src/components/Content/Content.tsx
@@ -4,6 +4,7 @@ import Test from "../Test/Test";
 import { useAppState } from "../../store/appState";
 import SignUp from "../Profile/SignUp";
 import Devices from "../Devices/Devices";
+import PlayerManagement from "../PlayerManagement/PlayerManagement";
 
 const Content: React.FC = () => {
   const activePage = useAppState((state) => state.activePage);
@@ -14,6 +15,7 @@ const Content: React.FC = () => {
       {activePage === "devices" && <Devices />}
       {activePage === "analytics" && <Test />}
       {activePage === "profile" && <SignUp />}
+      {activePage === "player-management" && <PlayerManagement />}
     </>
   );
 };
