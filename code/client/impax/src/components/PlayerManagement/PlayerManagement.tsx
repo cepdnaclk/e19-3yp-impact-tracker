0c2a944fbe1166cbd15acd05ae333667d685d2d7
kaushithamsilva <kaushithamsilva@gmail.com>
2023-12-28 16:45:13 +0530

    Added Player management screen

---
+++ code/client/impax/src/components/PlayerManagement/PlayerManagement.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Title from "../Title/Title";
+import { FaUsers } from "react-icons/fa";
+
+const PlayerManagement = () => {
+  return (
+    <main>
+      <Title title="Player Mangement" Icon={FaUsers} />
+    </main>
+  );
+};
+
+export default PlayerManagement;
