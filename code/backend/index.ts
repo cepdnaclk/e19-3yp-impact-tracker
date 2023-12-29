0b265b10229f0f51c29a12f35194a80f6b7246f6
Kanishka Gunawardana <kanishkagunawarthana@gmail.com>
2023-12-29 07:32:30 +0530

    Refactor backend index.js and index.ts files

--- code/backend/index.ts
+++ code/backend/index.ts
@@ -1,16 +1,16 @@
 // Import necessary libraries and modules
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import options from './src/config/corsOptions';
-import apiInfo from './src/config/config';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import options from "./src/config/corsOptions";
+import apiInfo from "./src/config/config";
 import swaggerUi from "swagger-ui-express";
 
 // Create an instance of the Express application
 const app: Express = express();
 
-import { connectToDatabase } from './src/db/connectdb'; // Adjust the path
+import { connectToDatabase } from "./src/db/connectdb"; // Adjust the path
 connectToDatabase();
 
 // Load environment variables from a .env file
@@ -40,7 +40,7 @@ import * as swaggerDocument from "./swagger.json";
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Define a basic route for the root endpoint
-app.get('/', (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.send(apiInfo.definition.info);
 });
 
