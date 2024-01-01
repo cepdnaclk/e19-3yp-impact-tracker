00e31da195dd676d43e1b1444f0e8d1cd1927f5b
kaushithamsilva <kaushithamsilva@gmail.com>
2024-01-01 22:24:03 +0530

    edit and remove player

--- code/client/impax/src/components/PlayerManagement/PlayersTable/PlayerActions/PlayerActions.tsx
+++ code/client/impax/src/components/PlayerManagement/PlayersTable/PlayerActions/PlayerActions.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Btn from "../../../Buttons/Btn";
-import { FaEdit, FaTrash } from "react-icons/fa";
+import { FaCross, FaEdit, FaTimes, FaTrash } from "react-icons/fa";
 import styles from "./PlayerActions.module.scss";
+import AlertModal from "../../../Modal/AlertModal";
+
 const PlayerActions: React.FC<{ jerseyId: number }> = ({ jerseyId }) => {
   return (
     <div className={styles.actions}>
@@ -12,14 +14,41 @@ const PlayerActions: React.FC<{ jerseyId: number }> = ({ jerseyId }) => {
       >
         edit
       </Btn>
-      <Btn
-        buttonStyle="secondary"
-        bgColor="rgba(255,255,255,0)"
-        Icon={FaTrash}
-        iconSizeEm={1.2}
-      >
-        remove
-      </Btn>
+      <AlertModal
+        trigger={
+          <Btn
+            buttonStyle="secondary"
+            bgColor="rgba(255,255,255,0)"
+            Icon={FaTrash}
+            iconSizeEm={1.2}
+          >
+            remove
+          </Btn>
+        }
+        title="Are you absolutely sure?"
+        description=" This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers."
+        cancel={
+          <Btn
+            Icon={FaTimes}
+            buttonStyle="primary"
+            iconSizeEm={1}
+            bgColor="rgb(54, 57, 59)"
+          >
+            Cancel
+          </Btn>
+        }
+        action={
+          <Btn
+            bgColor="transparent"
+            buttonStyle="secondary"
+            Icon={FaTrash}
+            iconSizeEm={1}
+          >
+            Remove
+          </Btn>
+        }
+      />
     </div>
   );
 };
