7738457ea6447ea9618285d269a390fdde52bb91
kaushithamsilva <kaushithamsilva@gmail.com>
2024-01-05 03:17:47 +0530

    Added todo

--- code/client/impax/src/components/Content/Content.tsx
+++ code/client/impax/src/components/Content/Content.tsx
@@ -8,7 +8,7 @@ import PlayerManagement from "../PlayerManagement/PlayerManagement";
 
 const Content: React.FC = () => {
   const activePage = useAppState((state) => state.activePage);
-
+  //TODO: If mqtt is offline, live and devices should be replaced by a screen that says "mqtt is offline"
   return (
     <>
       {activePage === "live" && <Live />}
