57292442c25f85d090bab98d6cc9adf66c919de2
Mansitha <iammansitha@gmail.com>
2024-01-05 23:38:01 +0530

    refactor

--- code/client/impax/src/App.tsx
+++ code/client/impax/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
     <>
       <Detector
         onChange={setIsInternetAvailable}
-        render={({ online }) => (
+        render={() => (
           <>
             <Sidebar />
             <Content />
