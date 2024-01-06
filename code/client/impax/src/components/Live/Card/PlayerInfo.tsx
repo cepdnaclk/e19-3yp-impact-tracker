796ff62005eb8f4d0083906f24283293eaca49a2
kaushithamsilva <kaushithamsilva@gmail.com>
2024-01-07 02:57:11 +0530

    refactored playerInfo to get all data from lookup

--- code/client/impax/src/components/Live/Card/PlayerInfo.tsx
+++ code/client/impax/src/components/Live/Card/PlayerInfo.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import styles from "./PlayerInfo.module.scss";
 import Battery from "../../Battery/Battery";
-type playerInfo = {
-  jerseyNo: number;
-  name: string;
-  device: string;
-};
+import { useAppState } from "../../../states/appState";
 
-const PlayerInfo: React.FC<{ playerInfo: playerInfo }> = ({ playerInfo }) => {
+const PlayerInfo: React.FC<{ buddy_id: number }> = ({ buddy_id }) => {
+  const jersey_no = useAppState((state) => state.playerMap[buddy_id]);
+  const playerInfo = useAppState((state) => state.playerDetails[jersey_no]);
+  const batteryLevel = useAppState(
+    (state) => state.buddiesStatus[buddy_id].battery
+  );
+
   return (
     <div className={styles.playerInfo}>
-      <p className={styles.jerseyNo}>{playerInfo.jerseyNo.toString()}</p>
+      <p className={styles.jerseyNo}>{jersey_no.toString()}</p>
       <div className={styles.name}>
         <p className={styles.playerName}>{playerInfo.name}</p>
         <div className={styles.device}>
-          Device {playerInfo.device} <Battery batteryLevel={60} />
+          Buddy #{buddy_id} <Battery batteryLevel={batteryLevel} />
         </div>
       </div>
     </div>
