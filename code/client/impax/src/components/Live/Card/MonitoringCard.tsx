e91764fc913b55a165084ad17c950c2569aaf5bc
kaushithamsilva <kaushithamsilva@gmail.com>
2024-01-07 02:58:33 +0530

    Refactored cards to only take in buddy_id and derive the details from global states

--- code/client/impax/src/components/Live/Card/MonitoringCard.tsx
+++ code/client/impax/src/components/Live/Card/MonitoringCard.tsx
@@ -2,44 +2,42 @@ import styles from "./Card.module.scss";
 import { MdClose } from "react-icons/md";
 import PlayerInfo from "./PlayerInfo";
 import MonitoringElements from "./MonitoringElements";
-
-type playerInfo = {
-  jerseyNo: number;
-  name: string;
-  device: string;
-};
+import { useAppState } from "../../../states/appState";
 
 type metrics = {
   speed: number;
   distance: number;
 };
 
-type latestImpact = {
-  value: number;
-  time: Date;
-  direction: "TOP" | "BOTTOM" | "LEFT" | "RIGHT" | "FRONT" | "BACK";
-};
+// type latestImpact = {
+//   value: number;
+//   time: Date;
+//   direction: "TOP" | "BOTTOM" | "LEFT" | "RIGHT" | "FRONT" | "BACK";
+// };
 
 interface MonitoringCardProps {
-  playerInfo: playerInfo;
-  metrics?: metrics;
-  latestImpact?: latestImpact;
-  totalImpact?: number;
+  buddy_id: number;
+  onClick: (buddy_id: number) => void;
 }
-const MonitoringCard = ({
-  playerInfo,
-  metrics,
-  latestImpact,
-  totalImpact,
-}: MonitoringCardProps) => {
+const MonitoringCard = ({ buddy_id, onClick }: MonitoringCardProps) => {
+  const metrics = {
+    speed: 10,
+    distance: 100,
+  } as metrics;
+
+  const buddyImpact = useAppState((state) => state.buddiesImpact[buddy_id]);
   return (
     <div className={styles.card}>
-      <MdClose className={styles.actionBtn} tabIndex={0} />
-      <PlayerInfo playerInfo={playerInfo} />
+      <MdClose
+        className={`${styles.actionBtn} ${styles.close}`}
+        tabIndex={0}
+        onClick={() => onClick(buddy_id)}
+      />
+      <PlayerInfo buddy_id={buddy_id} />
       <MonitoringElements
         metrics={metrics}
-        latestImpact={latestImpact}
-        totalImpact={totalImpact}
+        latestImpact={buddyImpact}
+        totalImpact={20}
       />
     </div>
   );
