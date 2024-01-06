426101ab07bab8b2e67919fe96d45dc103d2ab86
kaushithamsilva <kaushithamsilva@gmail.com>
2024-01-06 22:42:52 +0530

    global state changed to accomodate playerDetails and playerMap

--- code/client/impax/src/states/appState.ts
+++ code/client/impax/src/states/appState.ts
@@ -1,12 +1,14 @@
 import { create } from "zustand";
 import {
   Buddies,
-  BuddyStatus,
   activePage,
   BuddiesImpact,
-  Impact,
+  Players,
+  PlayerMap,
 } from "../types";
-import { Map } from "immutable";
+import { players } from "../data/players";
+import { deleteByValue } from "../utils/utils";
+import MqttClient from "../services/mqttClient";
 
 interface AppState {
   activePage: activePage;
@@ -23,6 +25,12 @@ interface AppState {
 
   buddiesImpact: BuddiesImpact;
   setBuddiesImpact: (buddiesImpact: BuddiesImpact) => void;
+
+  playerDetails: Players;
+
+  playerMap: PlayerMap;
+  setPlayerMap: (playerMap: PlayerMap) => void;
+  updatePlayerMap: (buddy_id: number, player_id: number) => void;
 }
 
 export const useAppState = create<AppState>()((set) => ({
@@ -39,12 +47,32 @@ export const useAppState = create<AppState>()((set) => ({
   setIsInternetAvailable: (isOnline) => set({ isInternetAvailable: isOnline }),
 
   //For the buddies status
-  buddiesStatus: Map<number, BuddyStatus>() as Buddies,
+  buddiesStatus: {} as Buddies,
   setBuddiesStatus: (buddiesState: Buddies) =>
     set({ buddiesStatus: buddiesState }),
 
   //For the buddies impact
-  buddiesImpact: Map<number, Impact>() as BuddiesImpact,
+  buddiesImpact: {} as BuddiesImpact,
   setBuddiesImpact: (buddiesImpact: BuddiesImpact) =>
     set({ buddiesImpact: buddiesImpact }),
+
+  //TODO: Clashing of players with other dashbaords
+  playerDetails: players,
+
+  //For the player map
+  playerMap: {} as PlayerMap,
+  setPlayerMap: (playerMap: PlayerMap) => set({ playerMap: playerMap }),
+  updatePlayerMap: (buddy_id: number, player_id: number) => {
+    set((prevState) => {
+      const playerMap = { ...prevState.playerMap };
+      //check if player_id is already in playerMap and delete it
+      deleteByValue(playerMap, player_id);
+
+      playerMap[buddy_id] = player_id;
+
+      //publish new playerMap to mqtt
+      MqttClient.getInstance().publishPlayerMap(playerMap);
+      return { playerMap };
+    });
+  },
 }));
