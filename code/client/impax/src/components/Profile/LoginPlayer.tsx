9a4e9da7bbc2a2acfe4c41ac7a335d9890850869
Mansitha <iammansitha@gmail.com>
2024-01-06 20:14:43 +0530

    Login manager component added

---
+++ code/client/impax/src/components/Profile/LoginPlayer.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import styles from "./SignUp.module.scss";
+import { Role } from "../../types";
+import { useRoleState, useSignupState } from "../../states/formState";
+const LoginPassword = () => {
+  interface formData {
+    role: Role;
+    teamId: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    password: string;
+    retypePassword: string;
+    teamName: string;
+  }
+  const [formData, setFormData] = useState<formData>({
+    role: "manager",
+    teamId: "",
+    email: "",
+    firstName: "",
+    lastName: "",
+    password: "",
+    retypePassword: "",
+    teamName: "",
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData); // Log the form data object
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.inputContainer}>
+          <label htmlFor="teamId">Team ID</label>
+          <input
+            type="text"
+            id="teamId"
+            required
+            placeholder="peradeniya-baseball"
+            value={formData.teamId}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className={styles.inputContainer}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            required
+            placeholder="johndoe@email.com"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className={styles.inputContainer}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            placeholder="Enter password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <button type="submit" className={styles.nextBtn}>
+          Signup
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default LoginPassword;
