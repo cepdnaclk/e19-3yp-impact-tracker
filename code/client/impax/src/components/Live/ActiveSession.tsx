bd3e25c1277232e0ffe3f0d2442509581338bb4b
kaushithamsilva <kaushithamsilva@gmail.com>
2024-01-07 03:00:18 +0530

    Active Session reconfigured to take in mqtt data and global states

--- code/client/impax/src/components/Live/ActiveSession.tsx
+++ code/client/impax/src/components/Live/ActiveSession.tsx
@@ -5,18 +5,45 @@ import Btn from "../Buttons/Btn";
 import MonitoringCard from "./Card/MonitoringCard";
 import ActiveCard from "./Card/ActiveCard";
 import AlertModal from "../Modal/AlertModal";
+import { useAppState } from "../../states/appState";
+
 const ActiveSession = () => {
+  const sessionDetails = useAppState((state) => state.sessionDetails);
+  const endSession = useAppState((state) => state.endSession);
+  const playerMap = useAppState((state) => state.playerMap);
+
+  const monitoringBuddies = useAppState((state) => state.monitoringBuddies);
+  const addToMonitoringBuddies = useAppState(
+    (state) => state.addToMonitoringBuddies
+  );
+  const removeFromMonitoringBuddies = useAppState(
+    (state) => state.removeFromMonitoringBuddies
+  );
+
+  const activeBuddies = new Set<number>(
+    Object.keys(playerMap)
+      .map((buddy_id) => parseInt(buddy_id))
+      .filter((x) => !monitoringBuddies.has(x))
+  );
+
+  const handleAddToMonitoring = (buddy_id: number) => {
+    addToMonitoringBuddies(buddy_id);
+  };
+
+  const handleRemoveFromMonitoring = (buddy_id: number) => {
+    removeFromMonitoringBuddies(buddy_id);
+  };
   //Time object of 15 mins and 5 mins before for testing
-  const currentDate = new Date();
-  const fifteenMinutesBefore = new Date(currentDate.getTime() - 15 * 60 * 1000);
-  const fiveMinutesBefore = new Date(currentDate.getTime() - 4 * 60 * 1000);
-  const eightMinutesBefore = new Date(currentDate.getTime() - 8 * 60 * 1000);
+  // const currentDate = new Date();
+  // const fifteenMinutesBefore = new Date(currentDate.getTime() - 15 * 60 * 1000);
+  // const fiveMinutesBefore = new Date(currentDate.getTime() - 4 * 60 * 1000);
+  // const eightMinutesBefore = new Date(currentDate.getTime() - 8 * 60 * 1000);
   return (
     <>
       <div className={styles.session}>
         <div className={styles.info}>
-          <h2>Practice Session at Main Ground</h2>
-          <span>Session #21</span>
+          <h2>{sessionDetails?.session_name}</h2>
+          <span>Session Id: {sessionDetails?.session_id}</span>
         </div>
         <div className={styles.controls}>
           <Btn Icon={FaEdit} buttonStyle="secondary">
@@ -25,85 +52,44 @@ const ActiveSession = () => {
           <AlertModal
             trigger={<Btn Icon={IoMdExit}>Exit Session</Btn>}
             title="Are you sure to exit session?"
-            description="sample"
-            action={<Btn Icon={IoMdExit}>Confirm Exit</Btn>}
+            description="Session data will be downloaded to your computer and uploaded when connected to the internet"
+            action={
+              <Btn Icon={IoMdExit} onClick={endSession}>
+                Confirm Exit
+              </Btn>
+            }
             cancel={<Btn Icon={FaTimes}>Cancel</Btn>}
           />
         </div>
       </div>
-      <div className={styles.monitoring}>
-        <h3>Monitoring Players</h3>
-        <div className={styles.grid}>
-          <MonitoringCard
-            key={69}
-            playerInfo={{
-              device: "#123",
-              jerseyNo: 69,
-              name: "Angelo Mathews",
-            }}
-            metrics={{
-              speed: 13,
-              distance: 200,
-            }}
-            latestImpact={{
-              value: 10,
-              direction: "TOP",
-              time: fifteenMinutesBefore,
-            }}
-            totalImpact={12}
-          />
-
-          <MonitoringCard
-            key={23}
-            playerInfo={{
-              device: "#112",
-              jerseyNo: 23,
-              name: "TM Dilshan",
-            }}
-            metrics={{
-              speed: 8,
-              distance: 450,
-            }}
-            latestImpact={{
-              value: 50,
-              direction: "FRONT",
-              time: fiveMinutesBefore,
-            }}
-            totalImpact={120}
-          />
-          <MonitoringCard
-            key={7}
-            playerInfo={{
-              device: "#12",
-              jerseyNo: 7,
-              name: "Dasun Shanaka",
-            }}
-            metrics={{
-              speed: 3,
-              distance: 50,
-            }}
-            latestImpact={{
-              value: 10,
-              direction: "TOP",
-              time: eightMinutesBefore,
-            }}
-            totalImpact={90}
-          />
+      {monitoringBuddies.size > 0 && (
+        <div className={styles.monitoring}>
+          <h3>Monitoring Players</h3>
+          <div className={styles.grid}>
+            {[...monitoringBuddies].map((buddy_id) => (
+              <MonitoringCard
+                key={buddy_id}
+                buddy_id={buddy_id}
+                onClick={handleRemoveFromMonitoring}
+              />
+            ))}
+          </div>
         </div>
-      </div>{" "}
-      <div className={styles.active}>
-        <h3>Active Players</h3>
-        <div className={styles.grid}>
-          <ActiveCard
-            key={11}
-            playerInfo={{
-              device: "#22",
-              jerseyNo: 11,
-              name: "Kumar Sangakkara",
-            }}
-          />
+      )}
+      {activeBuddies.size > 0 && (
+        <div className={styles.active}>
+          <h3>Active Players</h3>
+          <div className={styles.grid}>
+            {[...activeBuddies].map((buddy_id) => (
+              <ActiveCard
+                key={buddy_id}
+                buddy_id={buddy_id}
+                onClick={handleAddToMonitoring}
+              />
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
