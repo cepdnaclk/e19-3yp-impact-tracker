41bc77b55fd9e8a64e2355a00c98e910ad9deaab
kaushithamsilva <kaushithamsilva@gmail.com>
2024-01-06 16:35:33 +0530

    updated types

--- code/client/impax/src/types/index.d.ts
+++ code/client/impax/src/types/index.d.ts
@@ -1,11 +1,32 @@
-type buddyStatus = {
+import { Map } from "immutable";
+
+type BuddyStatus = {
   battery: number;
-  timestamp: Date;
+  timestamp: number;
 };
 
+type Impact = {
+  magntitude: number;
+  direction: "left" | "right" | "front" | "back" | "top" | "bottom";
+};
 export interface Buddies extends Map<number, BuddyStatus> {
   get(buddy_id: number): BuddyStatus | undefined;
   set(buddy_id: number, status: BuddyStatus): this;
   has(buddy_id: number): boolean;
-  delete(buddy_id: number): boolean;
+  delete(buddy_id: number): this;
 }
+
+export interface BuddiesImpact extends Map<number, Impact> {
+  get(buddy_id: number): Impact | undefined;
+  set(buddy_id: number, impact: Impact): this;
+  has(buddy_id: number): boolean;
+  delete(buddy_id: number): this;
+}
+
+export type activePage =
+  | "live"
+  | "devices"
+  | "analytics"
+  | "profile"
+  | "test"
+  | "player-management";
