96159a3368cba5125f96a3c8187193b319a630ec
Mansitha <iammansitha@gmail.com>
2024-01-07 13:35:50 +0530

    refactor

--- code/client/impax/src/components/Profile/LoginManager.tsx
+++ code/client/impax/src/components/Profile/LoginManager.tsx
@@ -1,46 +1,12 @@
-import React, { useState } from "react";
 import styles from "./SignUp.module.scss";
-import { Role } from "../../types";
-import { useRoleState, useSignupState } from "../../states/formState";
+import { useSignupState } from "../../states/formState";
 const LoginManager = () => {
   const isSignup = useSignupState((state) => state.isSignup);
   const setIsSignup = useSignupState((state) => state.setIsSignup);
-  interface formData {
-    role: Role;
-    teamId: string;
-    email: string;
-    firstName: string;
-    lastName: string;
-    password: string;
-    retypePassword: string;
-    teamName: string;
-  }
-  const [formData, setFormData] = useState<formData>({
-    role: "manager",
-    teamId: "",
-    email: "",
-    firstName: "",
-    lastName: "",
-    password: "",
-    retypePassword: "",
-    teamName: "",
-  });
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { id, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [id]: value,
-    }));
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(formData); // Log the form data object
-  };
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form>
         <div className={styles.inputContainer}>
           <label htmlFor="teamId">Team ID</label>
           <input
@@ -48,8 +14,6 @@ const LoginManager = () => {
             id="teamId"
             required
             placeholder="peradeniya-baseball"
-            value={formData.teamId}
-            onChange={handleInputChange}
           />
         </div>
         <div className={styles.inputContainer}>
@@ -59,8 +23,6 @@ const LoginManager = () => {
             id="email"
             required
             placeholder="johndoe@email.com"
-            value={formData.email}
-            onChange={handleInputChange}
           />
         </div>
         <div className={styles.inputContainer}>
@@ -70,8 +32,6 @@ const LoginManager = () => {
             id="password"
             required
             placeholder="Enter password"
-            value={formData.password}
-            onChange={handleInputChange}
           />
         </div>
 
