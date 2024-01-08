da65ef6b41c41961e709d6b9db36947017beb158
Mansitha <iammansitha@gmail.com>
2024-01-08 13:20:00 +0530

    test web serial api

--- code/client/impax/renderer.js
+++ code/client/impax/renderer.js
@@ -1,49 +1,97 @@
 const decoder = new TextDecoder();
+// async function testIt() {
+//   const filters = [
+//     { usbVendorId: 0x2341, usbProductId: 0x0043 },
+//     { usbVendorId: 0x2341, usbProductId: 0x0001 },
+//   ];
+//   try {
+//     // console.log(navigator.serial.getPorts()[0].getInfo());
+//     const ports = await navigator.serial.getPorts();
+
+//     // console.log(await navigator.serial.getPorts());
+//     console.log(ports);
+//     const port = await navigator.serial.requestPort({ filters });
+
+//     // console.log(ports);
+//     await port.open({ baudRate: 115200 });
+//     console.log("Port Opened", port);
+//     // Read Data
+//     // while (port.readable) {
+//     //   const reader = port.readable.getReader();
+//     //   try {
+//     //     while (true) {
+//     //       const { value, done } = await reader.read();
+//     //       if (done) {
+//     //         // |reader| has been canceled.
+//     //         break;
+//     //       }
+//     //       console.log(decoder.decode(value.buffer));
+//     //       // Do something with |value|...
+//     //     }
+//     //   } catch (error) {
+//     //     // Handle |error|...
+//     //   } finally {
+//     //     reader.releaseLock();
+//     //   }
+//     // }
+
+//     // Write Data
+
+//     // const encoder = new TextEncoder();
+//     // const writer = port.writable.getWriter();
+//     // await writer.write(new Uint8Array([65]));
+//     // writer.releaseLock();
+
+//     // Read Data
+//     // while (port.readable) {
+//     //   const reader = port.readable.getReader();
+//     //   try {
+//     //     while (true) {
+//     //       const { value, done } = await reader.read();
+//     //       if (done) {
+//     //         reader.releaseLock();
+//     //         // |reader| has been canceled.
+//     //         break;
+//     //       }
+//     //       // console.log(decoder.decode(value.buffer));
+//     //       // Do something with |value|...
+//     //       console.log(value);
+//     //     }
+//     //   } catch (error) {
+//     //     // Handle |error|...
+//     //   } finally {
+//     //     reader.releaseLock();
+//     //   }
+//     // }
+
+//     const portInfo = port.getInfo();
+//     // console.log(portInfo);
+//     document.getElementById(
+//       "device-name"
+//     ).innerHTML = `vendorId: ${portInfo.usbVendorId} | productId: ${portInfo.usbProductId} `;
+//   } catch (ex) {
+//     if (ex.name === "NotFoundError") {
+//       document.getElementById("device-name").innerHTML = "Device NOT found";
+//     } else {
+//       document.getElementById("device-name").innerHTML = ex;
+//     }
+//   }
+// }
+
+// document.getElementById("clickme").addEventListener("click", testIt);
 
 async function testIt() {
   const filters = [
     { usbVendorId: 0x2341, usbProductId: 0x0043 },
     { usbVendorId: 0x2341, usbProductId: 0x0001 },
   ];
+
   try {
-    // console.log(navigator.serial.getPorts()[0].getInfo());
-    // const ports = await navigator.serial.getPorts();
-
-    // console.log(ports);
     const port = await navigator.serial.requestPort({ filters });
-    // console.log(await navigator.serial.getPorts());
-
-    // console.log(ports);
+    // console.log(port);
     await port.open({ baudRate: 115200 });
-    console.log("Port Opened", port);
-    // Read Data
-    // while (port.readable) {
-    //   const reader = port.readable.getReader();
-    //   try {
-    //     while (true) {
-    //       const { value, done } = await reader.read();
-    //       if (done) {
-    //         // |reader| has been canceled.
-    //         break;
-    //       }
-    //       console.log(decoder.decode(value.buffer));
-    //       // Do something with |value|...
-    //     }
-    //   } catch (error) {
-    //     // Handle |error|...
-    //   } finally {
-    //     reader.releaseLock();
-    //   }
-    // }
+    console.log("Port Opened");
 
-    // Write Data
-
-    const encoder = new TextEncoder();
-    const writer = port.writable.getWriter();
-    await writer.write(new Uint8Array([65]));
-    writer.releaseLock();
-
-    // Read Data
     while (port.readable) {
       const reader = port.readable.getReader();
       try {
@@ -54,9 +102,9 @@ async function testIt() {
             // |reader| has been canceled.
             break;
           }
-          // console.log(decoder.decode(value.buffer));
+          console.log(decoder.decode(value.buffer));
           // Do something with |value|...
-          console.log(value);
+          // console.log(value);
         }
       } catch (error) {
         // Handle |error|...
@@ -66,7 +114,6 @@ async function testIt() {
     }
 
     const portInfo = port.getInfo();
-    // console.log(portInfo);
     document.getElementById(
       "device-name"
     ).innerHTML = `vendorId: ${portInfo.usbVendorId} | productId: ${portInfo.usbProductId} `;
