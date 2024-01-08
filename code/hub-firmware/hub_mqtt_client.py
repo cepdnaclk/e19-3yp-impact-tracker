4f1c89a8c614457202ebd32a97bd0e593fd50808
Sanduni Ubayasiri <115542118+SanduniU@users.noreply.github.com>
2024-01-08 22:46:08 +0530

    Fix timestamp calculation in hub_mqtt_client.py

--- code/hub-firmware/hub_mqtt_client.py
+++ code/hub-firmware/hub_mqtt_client.py
@@ -65,7 +65,7 @@ def on_message(client, userdata, msg):
             device_id = msg.topic.split("/")[1]
             if device_id in player_device_mapping:
                 player_id = player_device_mapping[device_id]
-                timestamp = int(time.time()*1000)+timestamp 
+                timestamp = int(time.time()*1000)+time_offset
                 impact_json = data[0]+' '+data[1]+' '+ str(timestamp)
                 print(impact_json)
                 
