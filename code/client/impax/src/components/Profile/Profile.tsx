5ce6e94e199d320f2ceaa0e859daf9867e323084
kaushithamsilva <kaushithamsilva@gmail.com>
2024-01-09 18:43:50 +0530

    Profile Screen Template

--- code/client/impax/src/components/Profile/Profile.tsx
+++ code/client/impax/src/components/Profile/Profile.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { IoMdExit } from "react-icons/io";
+import Btn from "../Buttons/Btn";
+import Title from "../Title/Title";
+import styles from "./Profile.module.scss";
+import { FaRegUserCircle } from "react-icons/fa";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <main>
+      <Title Icon={FaRegUserCircle} title="Profile" />
+      <div className={styles.loggedInStatus}>
+        <div className={styles.info}>
+          <h2>Team Name (Team Id)</h2>
+          <span>manager email: </span>
+        </div>
+        <div className={styles.controls}>
+          <Btn buttonStyle="primary" Icon={IoMdExit} onClick={() => {}}>
+            Log Out
+          </Btn>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Profile;
