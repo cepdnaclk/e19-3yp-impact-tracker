bf60d812f17c9c79dca746dcaf765adfa3aef4cb
kaushithamsilva <kaushithamsilva@gmail.com>
2024-01-09 18:58:54 +0530

    updated profile

--- code/client/impax/src/components/Profile/Profile.tsx
+++ code/client/impax/src/components/Profile/Profile.tsx
@@ -3,15 +3,22 @@ import Btn from "../Buttons/Btn";
 import Title from "../Title/Title";
 import styles from "./Profile.module.scss";
 import { FaRegUserCircle } from "react-icons/fa";
+import { useSignupState } from "../../states/formState";
 
 const Profile = () => {
+  // Get team-id
+  // Get team-name
+  // Get manager email
+
   return (
     <main>
       <Title Icon={FaRegUserCircle} title="Profile" />
       <div className={styles.loggedInStatus}>
         <div className={styles.info}>
-          <h2>Team Name (Team Id)</h2>
-          <span>manager email: </span>
+          <h2>
+            Sri Lanka A <span>(SL-A_2024)</span>
+          </h2>
+          <span>manager email: chrissiilverwood@gmail.com</span>
         </div>
         <div className={styles.controls}>
           <Btn buttonStyle="primary" Icon={IoMdExit} onClick={() => {}}>
