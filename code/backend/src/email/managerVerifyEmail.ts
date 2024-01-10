9dbee0551ec46444789fd28d2d48fd385debdc72
Kanishka Gunawardana <kanishkagunawarthana@gmail.com>
2024-01-10 08:24:05 +0530

    Refactor email template and update subject

--- code/backend/src/email/managerVerifyEmail.ts
+++ code/backend/src/email/managerVerifyEmail.ts
@@ -13,29 +13,30 @@ export async function sendVerificationEmail(
     },
   });
 
-  const signature = "IMPAX TEAM"; 
+  const signature = "IMPAX TEAM";
 
+  // <a href="http://16.170.235.219:5000/accept-invitation/${invitationToken}">Verify Email Here</a>
+  // <p>You've been created  the team ${teamName}! Click the following link to verify your email:</p>
   const mailOptions = {
     from: "impax.kmksh@gmail.com",
     to: recipientEmail,
-    subject: "Verification Email",
+    subject: "Notification Email",
     html: `
     <p>Hello,</p>
-    <p>You've been created  the team ${teamName}! Click the following link to verify your email:</p>
-    <a href="http://16.170.235.219:5000/accept-invitation/${invitationToken}">Verify Email Here</a>
+    <p>You've been created  the team ${teamName}!</p>
     <div>
       <img src="cid:Impax" alt="Impax Team" style="width: 100px; height: auto; max-width: 100%;" />
     </div>
     <p><i><b>${signature}</b></i></p>
     
   `,
-  attachments: [
-    {
-      filename: "Impax.jpeg.jpg",
-      path: "./src/email/Impax.jpeg", 
-      cid: "Impax", // same cid value as in the html img src
-    },
-  ],
+    attachments: [
+      {
+        filename: "Impax.jpeg.jpg",
+        path: "./src/email/Impax.jpeg",
+        cid: "Impax", // same cid value as in the html img src
+      },
+    ],
   };
 
   await transporter.sendMail(mailOptions);
