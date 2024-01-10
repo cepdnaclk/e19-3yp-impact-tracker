ae0a9a6c3906f136b7fc743c9b97d513b7e15499
hirushig <e19124@eng.pdn.ac.lk>
2024-01-10 08:06:51 +0530

    Verify&Invite emails modify

--- code/backend/src/email/managerInviteEmail.ts
+++ code/backend/src/email/managerInviteEmail.ts
@@ -2,7 +2,9 @@ import nodemailer from "nodemailer";
 
 export async function sendInvitationEmail(
   recipientEmail: string,
-  invitationToken: string
+  invitationToken: string,
+  teamName: string
+  
 ): Promise<void> {
 
   // Create a Nodemailer transporter
@@ -15,12 +17,31 @@ export async function sendInvitationEmail(
     },
   });
 
+  const signature = "IMPAX TEAM"; 
+
+
   // Compose the email
   const mailOptions = {
     from: "impax.kmksh@gmail.com",
     to: recipientEmail,
     subject: "Invitation to join the team",
-    text: `Click the following link to accept the invitation: https://5000/accept-invitation/${invitationToken}`,
+    html: `
+    <p>Hello,</p>
+    <p>You've been invited to join the team ${teamName}! Click the following link to accept the invitation:</p>
+    <a href="http://16.170.235.219:5000/accept-invitation/${invitationToken}">Accept Invitation</a>
+    <div>
+      <img src="cid:Impax" alt="Impax Team" style="width: 100px; height: auto; max-width: 100%;" />
+    </div>
+    <p><i><b>${signature}</b></i></p>
+    
+  `,
+  attachments: [
+    {
+      filename: "Impax.jpeg.jpg",
+      path: "./src/email/Impax.jpeg", 
+      cid: "Impax", // same cid value as in the html img src
+    },
+  ],
   };
 
   // Send the email
