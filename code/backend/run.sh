42e683de1560d590bbb73c4e7506c57d5c77a73b
Kanishka Gunawardana <kanishkagunawarthana@gmail.com>
2024-01-13 08:00:35 +0530

    Add run.sh script for managing backend server

---
+++ code/backend/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if backendserver is running
+if pm2 describe backendserver &> /dev/null; then
+    # If running, restart it
+    pm2 restart backendserver
+else
+    # If not running, start a new instance
+    pm2 start ./dist/index.js --name backendserver
+fi
