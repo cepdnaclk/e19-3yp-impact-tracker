4999acc4d3905d25d4db1e83190588d401545268
Mansitha <iammansitha@gmail.com>
2024-01-18 09:28:42 +0530

    Remove unused imports and state

--- code/client/impax/src/App.tsx
+++ code/client/impax/src/App.tsx
@@ -3,20 +3,9 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import MqttClient from "./services/mqttClient";
 import { Detector } from "react-detect-offline";
 import { HashRouter, Route, Routes } from "react-router-dom";
-import Live from "./components/Live/Live";
-import Devices from "./components/Devices/Devices";
-import Test from "./components/Test/Test";
-import PlayerManagement from "./components/PlayerManagement/PlayerManagement";
-import SignUp from "./components/Profile/SignUp";
-import TeamCreation from "./components/Profile/TeamCreation";
-import Success from "./components/StatusScreens/Success";
-import TeamExists from "./components/StatusScreens/TeamExists";
-import { useSignupState } from "./states/formState";
-import Profile from "./components/Profile/Profile";
 import routes from "./routes/routeConfig";
 function App() {
   MqttClient.getInstance();
-  const isLoggedIn = useSignupState((state) => state.isLoggedIn);
 
   return (
     <HashRouter>
