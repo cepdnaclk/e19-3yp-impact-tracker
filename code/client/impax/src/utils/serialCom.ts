f5301b27daca81af12d8bd203ffd70f82c42d4b4
Mansitha <iammansitha@gmail.com>
2024-01-18 09:01:38 +0530

    Refactor serialCom.ts: Rename functions, add syncDevice function

--- code/client/impax/src/utils/serialCom.ts
+++ code/client/impax/src/utils/serialCom.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-constant-condition */
-export async function sendMessage(
+export async function sendData(
     message: string,
     port: SerialPort,
     encoder: TextEncoder
@@ -15,7 +15,7 @@ export async function sendMessage(
     }
   }
   
-export async function readMessage(port: SerialPort, decoder: TextDecoder) {
+export async function readData(port: SerialPort, decoder: TextDecoder) {
     let message = "";
     while (port.readable) {
       const reader = port.readable.getReader();
@@ -40,3 +40,53 @@ export async function readMessage(port: SerialPort, decoder: TextDecoder) {
     } // Remove potential trailing newline
     return message;
   }
+
+  export const syncDevice = async () => {
+    const decoder = new TextDecoder();
+    const encoder = new TextEncoder();
+    const filters = [
+      { usbVendorId: 0x2341, usbProductId: 0x0043 },
+      { usbVendorId: 0x2341, usbProductId: 0x0001 },
+    ];
+    const filtersESP = [{ usbVendorId: 0x1a86, usbProductId: 0x7523 }];
+    if ("serial" in navigator) {
+      console.log("Yahooo Serial is supported");
+      const port = await (navigator.serial as Serial).requestPort({
+        filters: filters,
+      });
+      console.log(port);
+      await port.open({ baudRate: 9600 });
+
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      const reqMessage = "{impax,impax12345678,impax,impax}";
+
+      try {
+        await sendData("request", port, encoder);
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+        const ackMessage = await readData(port, decoder);
+        console.log("First Reply" + ackMessage);
+        if (ackMessage === "ack") {
+          await new Promise((resolve) => setTimeout(resolve, 3000));
+          await sendData("wificonfig", port, encoder);
+          await new Promise((resolve) => setTimeout(resolve, 3000));
+
+          const secondreply = await readData(port, decoder);
+          console.log("Second reply " + secondreply);
+          if (secondreply === "ack") {
+            console.log("Configuration sent successfully");
+          } else {
+            console.log("Configuration Not Sent!!");
+          }
+        } else {
+          console.error("Handshake failed: ACK not received");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      } finally {
+        await port.close();
+        console.log("Port closed");
+      }
+    } else {
+      console.log("its not");
+    }
+  };
