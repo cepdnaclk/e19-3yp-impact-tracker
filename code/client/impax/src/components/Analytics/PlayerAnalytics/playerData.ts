bc013deeceebef7a4766f7156df933792af37b67
kaushithamsilva <kaushithamsilva@gmail.com>
2024-01-18 23:02:16 +0530

    chart demo

---
+++ code/client/impax/src/components/Analytics/PlayerAnalytics/playerData.ts
@@ -0,0 +1,28 @@
+export const data: {
+  title: string;
+  value: string;
+  trend: number;
+  metaUnits?: string;
+}[] = [
+  {
+    title: "Cumulative Impacts",
+    value: "850 g",
+    trend: -20,
+  },
+  {
+    title: "Impacts Recorded",
+    value: "50",
+    trend: 20,
+  },
+  {
+    title: "Impact Frequency",
+    value: "400",
+    metaUnits: "/hr",
+    trend: 10,
+  },
+  {
+    title: "Average Impact",
+    value: "43 g",
+    trend: 40,
+  },
+];
