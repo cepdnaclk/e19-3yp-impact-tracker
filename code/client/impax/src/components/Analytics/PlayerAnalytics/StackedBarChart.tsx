500c2ab9bcdec1ebfe01b855bf884f76cc17b442
kaushithamsilva <kaushithamsilva@gmail.com>
2024-01-19 19:33:50 +0530

    chartjs

--- code/client/impax/src/components/Analytics/PlayerAnalytics/StackedBarChart.tsx
+++ code/client/impax/src/components/Analytics/PlayerAnalytics/StackedBarChart.tsx
@@ -1,83 +1,84 @@
-import React, { PureComponent } from "react";
-import {
-  BarChart,
-  Bar,
-  Cell,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-} from "recharts";
-
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import "chart.js/auto";
+import { ChartOptions } from "chart.js";
 
 export const StackedBarChart = () => {
-  return (
-    <ResponsiveContainer width="100%" height="100%">
-      <BarChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 20,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid stroke="5" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip cursor={{ fill: "black" }} />
-        <Legend />
-        <Bar dataKey="pv" stackId="a" fill="#8884d8" />
-        <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
-      </BarChart>
-    </ResponsiveContainer>
-  );
+  const xVals = [10, 30, 50, 70, 90, 110, 130, 150, 170, 190];
+  const yVals = [5, 8, 24, 16, 32, 42, 30, 17, 11];
+  const data = xVals.map((k, i) => ({ x: k, y: yVals[i] }));
+
+  const backgroundColor = Array(xVals.length).fill("rgba(255, 99, 132, 0.2)");
+  const borderColor = Array(xVals.length).fill("rgba(255, 99, 132, 1)");
+
+  // Prepare the data for the chart
+  const chartData = {
+    datasets: [
+      {
+        label: "Num of Visitors",
+        data: data,
+        backgroundColor: backgroundColor,
+        borderColor: borderColor,
+        borderWidth: 1,
+        barPercentage: 1,
+        categoryPercentage: 1,
+        borderRadius: 5,
+      },
+    ],
+  };
+
+  // Define the chart options
+
+  const chartOptions: ChartOptions<"bar"> = {
+    scales: {
+      x: {
+        type: "linear",
+        offset: false,
+        grid: {
+          offset: false,
+          display: true,
+        },
+        ticks: {
+          stepSize: 20,
+        },
+        title: {
+          display: true,
+          text: "Hours",
+          font: {
+            size: 14,
+          },
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: "Visitors",
+          font: {
+            size: 14,
+          },
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        display: true,
+      },
+      tooltip: {
+        callbacks: {
+          title: (items) => {
+            if (!items.length) {
+              return "";
+            }
+            const item = items[0];
+            const x = item.parsed.x;
+            const min = x - 10;
+            const max = x + 10;
+            return `Magnitude: ${min} - ${max}`;
+          },
+        },
+      },
+    },
+  };
+
+  return <Bar data={chartData} options={chartOptions} />;
 };
