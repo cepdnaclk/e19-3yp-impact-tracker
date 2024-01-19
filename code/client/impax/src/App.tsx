8ae9ef1b9ee86174f7e4e3fd6a0ca184a090b01d
Mansitha <iammansitha@gmail.com>
2024-01-19 16:36:42 +0530

    refactor: internet availability

--- code/client/impax/src/App.tsx
+++ code/client/impax/src/App.tsx
@@ -4,32 +4,39 @@ import MqttClient from "./services/mqttClient";
 import { Detector } from "react-detect-offline";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import routes from "./routes/routeConfig";
+import { useAppState } from "./states/appState";
+import { uploadSession } from "./services/httpClient";
+
 function App() {
   MqttClient.getInstance();
 
   return (
     <HashRouter>
       <Detector
-        render={({ online }) => (
-          <>
-            <Sidebar isOnline={online} />
-            <Routes>
-              {routes.map((route, index) => (
-                <Route
-                  key={index}
-                  path={route.path}
-                  element={
-                    route.props ? (
-                      <route.component {...route.props} />
-                    ) : (
-                      <route.component />
-                    )
-                  }
-                />
-              ))}
-            </Routes>
-          </>
-        )}
+        render={({ online }) => {
+          if (online) uploadSession();
+          useAppState.setState({ isInternetAvailable: online });
+          return (
+            <>
+              <Sidebar />
+              <Routes>
+                {routes.map((route, index) => (
+                  <Route
+                    key={index}
+                    path={route.path}
+                    element={
+                      route.props ? (
+                        <route.component {...route.props} />
+                      ) : (
+                        <route.component />
+                      )
+                    }
+                  />
+                ))}
+              </Routes>
+            </>
+          );
+        }}
       />
     </HashRouter>
   );
