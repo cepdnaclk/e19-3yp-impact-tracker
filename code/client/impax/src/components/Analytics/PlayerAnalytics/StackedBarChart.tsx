b20cb9cb4bd4412e799a08457893d18d6cacc32a
kaushithamsilva <kaushithamsilva@gmail.com>
2024-01-20 05:57:27 +0530

    chartjs for stacked bar chart

--- code/client/impax/src/components/Analytics/PlayerAnalytics/StackedBarChart.tsx
+++ code/client/impax/src/components/Analytics/PlayerAnalytics/StackedBarChart.tsx
@@ -5,20 +5,77 @@ import { ChartOptions } from "chart.js";
 
 export const StackedBarChart = () => {
   const xVals = [10, 30, 50, 70, 90, 110, 130, 150, 170, 190];
-  const yVals = [5, 8, 24, 16, 32, 42, 30, 17, 11];
-  const data = xVals.map((k, i) => ({ x: k, y: yVals[i] }));
 
-  const backgroundColor = Array(xVals.length).fill("rgba(255, 99, 132, 0.2)");
-  const borderColor = Array(xVals.length).fill("rgba(255, 99, 132, 1)");
+  const yValsLeft = [5, 8, 24, 16, 32, 42, 30, 17, 11, 2];
+  const arraySize = yValsLeft.length;
+
+  const yValsRight = Array.from({ length: arraySize }, () =>
+    Math.floor(Math.random() * 50)
+  );
+  const yValsFront = Array.from({ length: arraySize }, () =>
+    Math.floor(Math.random() * 50)
+  );
+  const yValsBack = Array.from({ length: arraySize }, () =>
+    Math.floor(Math.random() * 50)
+  );
+
+  const dataLeft = xVals.map((k, i) => ({ x: k, y: yValsLeft[i] }));
+  const dataRight = xVals.map((k, i) => ({ x: k, y: yValsRight[i] }));
+  const dataFront = xVals.map((k, i) => ({ x: k, y: yValsFront[i] }));
+  const dataBack = xVals.map((k, i) => ({ x: k, y: yValsBack[i] }));
+
+  const backgroundColorLeft = Array(xVals.length).fill(
+    "rgba(255, 99, 180, 0.2)"
+  );
+  const backgroundColorRight = Array(xVals.length).fill("rgb(65, 22, 38)");
+  const backgroundColorFront = Array(xVals.length).fill("rgb(46, 22, 38)");
+  const backgroundColorBack = Array(xVals.length).fill(
+    "rgba(104, 31, 58, 0.5)"
+  );
+
+  const borderColorLeft = Array(xVals.length).fill("rgba(255, 99, 132, 1)");
+  const borderColorRight = Array(xVals.length).fill("rgba(255, 99, 132, 1)");
+  const borderColorFront = Array(xVals.length).fill("rgba(255, 99, 132, 1)");
+  const borderColorBack = Array(xVals.length).fill("rgba(255, 99, 132, 1)");
 
   // Prepare the data for the chart
   const chartData = {
     datasets: [
       {
-        label: "Num of Visitors",
-        data: data,
-        backgroundColor: backgroundColor,
-        borderColor: borderColor,
+        label: "Left",
+        data: dataLeft,
+        backgroundColor: backgroundColorLeft,
+        borderColor: borderColorLeft,
+        borderWidth: 1,
+        barPercentage: 1,
+        categoryPercentage: 1,
+        borderRadius: 5,
+      },
+      {
+        label: "Right",
+        data: dataRight,
+        backgroundColor: backgroundColorRight,
+        borderColor: borderColorRight,
+        borderWidth: 1,
+        barPercentage: 1,
+        categoryPercentage: 1,
+        borderRadius: 5,
+      },
+      {
+        label: "Front",
+        data: dataFront,
+        backgroundColor: backgroundColorFront,
+        borderColor: borderColorFront,
+        borderWidth: 1,
+        barPercentage: 1,
+        categoryPercentage: 1,
+        borderRadius: 5,
+      },
+      {
+        label: "Back",
+        data: dataBack,
+        backgroundColor: backgroundColorBack,
+        borderColor: borderColorBack,
         borderWidth: 1,
         barPercentage: 1,
         categoryPercentage: 1,
@@ -30,8 +87,10 @@ export const StackedBarChart = () => {
   // Define the chart options
 
   const chartOptions: ChartOptions<"bar"> = {
+    maintainAspectRatio: false,
     scales: {
       x: {
+        stacked: true,
         type: "linear",
         offset: false,
         grid: {
@@ -43,25 +102,33 @@ export const StackedBarChart = () => {
         },
         title: {
           display: true,
-          text: "Hours",
+          text: "Magnitude",
           font: {
-            size: 14,
+            size: 16,
           },
         },
       },
       y: {
+        stacked: true,
         title: {
           display: true,
-          text: "Visitors",
+          text: "Num of impacts",
           font: {
-            size: 14,
+            size: 16,
           },
         },
       },
     },
     plugins: {
+      // title: {
+      //   display: true,
+      //   text: "Impact Histogram",
+      //   font: {
+      //     size: 16,
+      //   },
+      // },
       legend: {
-        display: true,
+        display: false,
       },
       tooltip: {
         callbacks: {
