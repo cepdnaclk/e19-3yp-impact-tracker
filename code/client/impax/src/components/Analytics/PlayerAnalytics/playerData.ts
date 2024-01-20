6953ff56f8cf4855c94dc9d79359b001c3fc47c9
Mansitha <iammansitha@gmail.com>
2024-01-20 23:56:15 +0530

    dummy player analytics data

--- code/client/impax/src/components/Analytics/PlayerAnalytics/playerData.ts
+++ code/client/impax/src/components/Analytics/PlayerAnalytics/playerData.ts
@@ -27,6 +27,35 @@ export const data: {
   },
 ];
 
+export const data2: {
+  title: string;
+  value: string;
+  trend: number;
+  metaUnits?: string;
+}[] = [
+  {
+    title: "Cumulative Impacts",
+    value: "199 g",
+    trend: -10,
+  },
+  {
+    title: "Impacts Recorded",
+    value: "987",
+    trend: 29,
+  },
+  {
+    title: "Impact Frequency",
+    value: "899",
+    metaUnits: "/hr",
+    trend: 69,
+  },
+  {
+    title: "Average Impact",
+    value: "43 g",
+    trend: 40,
+  },
+];
+
 export const criticalSessions: {
   name: string;
   date: string;
@@ -53,6 +82,36 @@ export const criticalSessions: {
     date: "06 Dec 2023",
     cumulative: 100,
     average: 25,
+    highest: 30,
+  },
+];
+
+export const criticalSessions2: {
+  name: string;
+  date: string;
+  cumulative: number;
+  average: number;
+  highest: number;
+}[] = [
+  {
+    name: "Sri SRI vs England",
+    date: "12 Jan 2024",
+    cumulative: 560,
+    average: 99,
+    highest: 96,
+  },
+  {
+    name: "Sri SRI vs Zimbabwe",
+    date: "23 Dec 2023",
+    cumulative: 400,
+    average: 25,
+    highest: 89,
+  },
+  {
+    name: "Sri SRI vs India",
+    date: "06 Dec 2023",
+    cumulative: 100,
+    average: 458,
     highest: 30,
   },
 ];
