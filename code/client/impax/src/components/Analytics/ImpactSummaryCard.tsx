3fc73253f9073351efe9f3a9ba87518a5c67f26d
kaushithamsilva <kaushithamsilva@gmail.com>
2024-01-20 20:12:51 +0530

    impactSummaryCard refactored into new component

---
+++ code/client/impax/src/components/Analytics/ImpactSummaryCard.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import cardStyles from "./ImpactSummaryCard.module.scss";
+import { Metric, TimeSpan } from "../../types";
+import { FaArrowTrendDown, FaArrowTrendUp } from "react-icons/fa6";
+
+const ImpactSummaryCard: React.FC<{ metric: Metric; timeSpan: TimeSpan }> = ({
+  metric,
+  timeSpan,
+}) => {
+  return (
+    <div className={cardStyles.card}>
+      <h3>{metric.title}</h3>
+      <p className={cardStyles.value}>
+        {metric.value}
+        {metric.metaUnits && (
+          <span className={cardStyles.metaUnits}>{metric.metaUnits}</span>
+        )}
+      </p>
+      {timeSpan != "All Time" && metric.trend && (
+        <p className={cardStyles.trend}>
+          {metric.trend > 0 ? (
+            <FaArrowTrendUp className={cardStyles.iconUp} />
+          ) : (
+            <FaArrowTrendDown className={cardStyles.iconDown} />
+          )}
+          <span>{metric.trend}%</span>
+          <span className={cardStyles.duration}>vs Last Week</span>
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default ImpactSummaryCard;
