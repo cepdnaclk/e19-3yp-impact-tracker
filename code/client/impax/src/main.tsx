8a25ce50cc91f07ca96350d3a20598f2d47cf930
Mansitha <iammansitha@gmail.com>
2024-01-20 23:54:26 +0530

    add react query client provider

--- code/client/impax/src/main.tsx
+++ code/client/impax/src/main.tsx
@@ -1,11 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
-
+import { QueryClient, QueryClientProvider } from "react-query";
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
-  <App />
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
   // </React.StrictMode>
 );
 
