99ec62c0110e399a6480e518fd52d9e8928102ef
kaushithamsilva <kaushithamsilva@gmail.com>
2024-01-21 10:31:12 +0530

    manager table

---
+++ code/client/impax/src/components/Profile/ManagersTable/managersData.ts
@@ -0,0 +1,14 @@
+import { Manager } from "../../../types";
+
+export const managers: Manager[] = [
+  {
+    name: "Kaushitha Silva",
+    email: "kaushithamsilva@gmail.com",
+    verification: "pending",
+  },
+  {
+    name: "Mansitha Eashwara",
+    email: "mansitha@gmail.com",
+    verification: "verified",
+  },
+];
