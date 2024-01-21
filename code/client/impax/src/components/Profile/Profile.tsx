4891dfee8daa7a1b0259a7e56f8932fa31fceb33
kaushithamsilva <kaushithamsilva@gmail.com>
2024-01-21 10:30:44 +0530

    Profile added manager

--- code/client/impax/src/components/Profile/Profile.tsx
+++ code/client/impax/src/components/Profile/Profile.tsx
@@ -5,6 +5,10 @@ import styles from "./Profile.module.scss";
 import { FaRegUserCircle } from "react-icons/fa";
 import { useLoginState } from "../../states/profileState";
 import { useSignupState } from "../../states/formState";
+import ManagersTable from "./ManagersTable/ManagersTable";
+import DialogModal from "../Modal/DialogModal";
+import { FaPlus } from "react-icons/fa6";
+import { useState } from "react";
 
 const Profile = () => {
   // Get team-id
@@ -13,6 +17,8 @@ const Profile = () => {
   const setIsLoggedIn = useSignupState((state) => state.setIsLoggedIn);
   const loginInfo = useLoginState((state) => state.loginInfo);
   const setLoginInfo = useLoginState((state) => state.setLoginInfo);
+
+  const [addManagerOpen, setAddManagerOpen] = useState<boolean>(false);
   // TODO: Stay logged in for 90 days and so much more
   return (
     <main>
@@ -43,8 +49,55 @@ const Profile = () => {
       </div>
       <div className={styles.gridLayout}>
         <div className={styles.managersContainer}>
-          <h2>Add/Remove Managers</h2>
-          <div className={styles.managersTableContainer}></div>
+          <div className={styles.info}>
+            <h2>Add/Remove Managers</h2>
+            <DialogModal
+              open={addManagerOpen}
+              setOpen={setAddManagerOpen}
+              trigger={
+                <Btn buttonStyle="primary" Icon={FaPlus} iconSizeEm={0.8}>
+                  Add New Manager
+                </Btn>
+              }
+              title="Add New Manager"
+              description="Add a new manager to your team, once added manager can create their account under your team id"
+            >
+              <form
+                className={styles.addManagerForm}
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  setAddManagerOpen(false);
+                }}
+              >
+                <label htmlFor="manager_name">Manager Name</label>
+                <input
+                  type="text"
+                  name="manager_name"
+                  id="manager_name"
+                  placeholder="Johnathan Doe"
+                />
+
+                <label htmlFor="email">
+                  Manager's Email
+                  <span className={styles.additionalInfo}>
+                    Link Impax Account
+                  </span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="johndoe@gmail.com"
+                />
+                <Btn type="submit" Icon={FaPlus}>
+                  Invite Manager
+                </Btn>
+              </form>
+            </DialogModal>
+          </div>
+          <div className={styles.managersTableContainer}>
+            <ManagersTable />
+          </div>
         </div>
       </div>
     </main>
