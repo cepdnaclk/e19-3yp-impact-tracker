bf36bbbefba5b8f2849b249a9d6cebbfcc62d77d
Mansitha <iammansitha@gmail.com>
2024-01-21 13:22:49 +0530

    new package

--- code/client/impax/src/main.tsx
+++ code/client/impax/src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
