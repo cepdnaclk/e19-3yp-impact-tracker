2ee2270746c59f91f0e83166c30dd67abbe5fe97
kaushithamsilva <kaushithamsilva@gmail.com>
2024-01-23 01:34:53 +0530

    batteryCharging animation

--- code/client/impax/src/components/Battery/Battery.tsx
+++ code/client/impax/src/components/Battery/Battery.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 import styles from "./Battery.module.scss";
 const Battery: React.FC<{ batteryLevel: number }> = ({ batteryLevel }) => {
-  if (batteryLevel < 0 || batteryLevel > 100)
+  if (batteryLevel != 200 && (batteryLevel < 0 || batteryLevel > 100))
     throw new Error("Battery Level should be in range(0,100)");
 
   //Change color of battery according to level
   let containerClasses = `${styles.batteryContainer} `;
-  if (batteryLevel > 50) containerClasses += `${styles.good}`;
+  if (batteryLevel > 100) containerClasses += `${styles.charging}`;
+  else if (batteryLevel > 50) containerClasses += `${styles.good}`;
   else if (batteryLevel > 20) containerClasses += `${styles.medium}`;
   else containerClasses += `${styles.low}`;
 
   return (
     <div className={containerClasses}>
       <div className={styles.batteryOuter}>
-        <div
-          className={styles.batteryLevel}
-          style={{ width: `${batteryLevel}%` }}
-        ></div>
+        {batteryLevel == 200 ? (
+          <div className={styles.batteryLevel}></div>
+        ) : (
+          <div
+            className={styles.batteryLevel}
+            style={{ width: `${batteryLevel}%` }}
+          ></div>
+        )}
       </div>
       <div className={styles.batteryBump}></div>
     </div>
