facbd49e57e743763ab58ac6c5bf5e581021c52a
kaushithamsilva <kaushithamsilva@gmail.com>
2024-01-27 23:06:55 +0530

    added key to skeleton

--- code/client/impax/src/components/Analytics/ImpactSummarySkeleton.tsx
+++ code/client/impax/src/components/Analytics/ImpactSummarySkeleton.tsx
@@ -3,8 +3,8 @@ const ImpactSummarySkeleton = () => {
   return (
     <div className={styles.skeleton}>
       <div className={styles.skeletonGrid}>
-        {[0, 1, 2, 3].map(() => (
-          <div className={styles.skeletonCard}>
+        {[0, 1, 2, 3].map((index) => (
+          <div className={styles.skeletonCard} key={index}>
             <h3></h3>
             <p className={styles.value}></p>
             <p className={styles.trend}></p>
