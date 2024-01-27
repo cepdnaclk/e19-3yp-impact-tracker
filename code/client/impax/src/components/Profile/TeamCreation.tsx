6f231585282ec5c210ec522a44cb5c6e64ce33b0
Mansitha <iammansitha@gmail.com>
2024-01-27 12:21:49 +0530

    Update errorPopup import in TeamCreation component

--- code/client/impax/src/components/Profile/TeamCreation.tsx
+++ code/client/impax/src/components/Profile/TeamCreation.tsx
@@ -4,7 +4,7 @@ import Hero from "./Hero";
 import { FieldValues, useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../../config/config";
-import { showPopup } from "../../utils/errorPopup.ts";
+import { showPopup } from "../../utils/popup.ts";
 
 const TeamCreation = () => {
   const setIsSignup = useSignupState((state) => state.setIsSignup);
@@ -43,7 +43,7 @@ const TeamCreation = () => {
     if (response.ok) {
       navigate("/signup/manager/success");
     } else {
-      await showPopup("Invalid Credentials", "Please Try Again");
+      await showPopup("error", "Invalid Credentials", "Please Try Again");
     }
     reset();
   };
