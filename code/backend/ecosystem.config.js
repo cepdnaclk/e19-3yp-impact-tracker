7b8e09e664f845c442b406ab460e1793ba40db3b
Kanishka Gunawardana <83855665+KATTA-00@users.noreply.github.com>
2024-01-27 14:42:12 +0530

    Update ecosystem.config.js

--- code/backend/ecosystem.config.js
+++ code/backend/ecosystem.config.js
@@ -5,5 +5,14 @@ module.exports = {
     watch: true,
     ignore_watch: ["node_modules"],
     restart_delay: 1000,
-  }]
+  }],
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: "13.235.86.1",
+      ref: "origin/deploy",
+      repo: "git@github.com:cepdnaclk/e19-3yp-impact-tracker.git",
+      path: "/code/backend",
+      "post-deploy": "npm install && npm run build && pm2 reload ecosystem.config.js --env production",
+    },
 };
