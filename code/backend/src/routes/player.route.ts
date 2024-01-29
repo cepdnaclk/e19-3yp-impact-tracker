622b3fcb1e3856cb8202c505dd65f6e587ab54db
hirushig <e19124@eng.pdn.ac.lk>
2024-01-29 12:48:48 +0530

    Update user authentication logic

--- code/backend/src/routes/player.route.ts
+++ code/backend/src/routes/player.route.ts
@@ -5,7 +5,10 @@ import { validateEmail } from "../utils/utils";
 import playerController from "../controllers/player.controller";
 import PlayerModel from "../db/player.schema";
 import PlayerTeamModel from "../db/players.in.team.schema";
-import { PlayerInTeamResponse, PlayerTeamRequest } from "../models/player.model";
+import {
+  PlayerInTeamResponse,
+  PlayerTeamRequest,
+} from "../models/player.model";
 
 // Create an instance of the Express Router
 const router = Router();
@@ -38,7 +41,7 @@ router.post("/add", async (req: Request, res: Response) => {
       fullName,
       newPlayerEmail,
       teamId,
-      managerEmail,
+      managerEmail
     );
 
     // This is just for adding playes with verification status
@@ -51,12 +54,10 @@ router.post("/add", async (req: Request, res: Response) => {
     //   isVerified: isVerified,
     // });
 
-
     // Save the manager to the database
     // const savedManager = await playerTeamInstance.save();
 
     res.send(playerInTeamResponse);
-
   } catch (err) {
     if (err instanceof Error) {
       // If 'err' is an instance of Error, send the error message
@@ -89,13 +90,12 @@ router.post("/", async (req: Request, res: Response) => {
   }
 
   try {
-    const playerResponse = await playerController.createPlayer( email, password);
+    const playerResponse = await playerController.createPlayer(email, password);
     if (playerResponse) {
       res.send(playerResponse);
     } else {
       res.send({ message: "Player create new account failed" });
     }
-    
   } catch (err) {
     if (err instanceof Error) {
       // If 'err' is an instance of Error, send the error message
@@ -116,9 +116,7 @@ router.get("/", async (req: Request, res: Response) => {
   }
   try {
     // Get Player details
-    const playerResponse = await playerController.getPlayer(
-      req.body.userName
-    );
+    const playerResponse = await playerController.getPlayer(req.body.userName);
 
     res.send(playerResponse);
   } catch (err) {
@@ -138,18 +136,18 @@ router.put("/update", async (req: Request, res: Response) => {
   const jerseyId = req.body.jerseyId;
   const fullName = req.body.fullName;
   const managerEmail = req.body.userName; // extract from middleware
-  const teamId = req.body.teamId;         // extract from middleware
+  const teamId = req.body.teamId; // extract from middleware
 
   // Check if any required field is missing
-  if (!jerseyId || !fullName || !newPlayerEmail ) {
+  if (!jerseyId || !fullName || !newPlayerEmail) {
     const missingFields = [];
-  if (!jerseyId) missingFields.push("jerseyId");
-  if (!fullName) missingFields.push("fullName");
-  if (!newPlayerEmail) missingFields.push("newPlayerEmail");
+    if (!jerseyId) missingFields.push("jerseyId");
+    if (!fullName) missingFields.push("fullName");
+    if (!newPlayerEmail) missingFields.push("newPlayerEmail");
 
-  const errorMessage = `Missing required fields: ${missingFields.join(", ")}`;
-  console.log(errorMessage);
-  res.status(HttpCode.BAD_REQUEST).send({ message: errorMessage });
+    const errorMessage = `Missing required fields: ${missingFields.join(", ")}`;
+    console.log(errorMessage);
+    res.status(HttpCode.BAD_REQUEST).send({ message: errorMessage });
   }
 
   // Validate email format
@@ -160,11 +158,12 @@ router.put("/update", async (req: Request, res: Response) => {
   }
 
   try {
-    const player = await PlayerTeamModel.find({ 
+    const player = await PlayerTeamModel.find({
       jesryId: jerseyId,
-      teamId: teamId });
+      teamId: teamId,
+    });
 
-      console.log(player );
+    console.log(player);
 
     const playerTeamRequest = new PlayerTeamRequest(
       newPlayerEmail,
@@ -172,15 +171,14 @@ router.put("/update", async (req: Request, res: Response) => {
       fullName
     );
 
-      
     let playerInTeamResponse;
 
-    if (player){
-
+    if (player.length === 1) {
       playerInTeamResponse = await playerController.updatePlayer(
-        playerTeamRequest, 
-        managerEmail, 
-        teamId);
+        playerTeamRequest,
+        managerEmail,
+        teamId
+      );
 
       // if (player.playerEmail != newPlayerEmail) {
       //   // Player with the same email already exists, update the existing player
@@ -197,16 +195,13 @@ router.put("/update", async (req: Request, res: Response) => {
       //   res.send({ message: "Player created successfully", playerInTeamResponse });
       // }
 
-
-      res.send({ message: "Player updated successfully", playerInTeamResponse });
-
-    }else{
+      res.send({
+        message: "Player updated successfully",
+        playerInTeamResponse,
+      });
+    } else {
       throw new Error(HttpMsg.PLAYER_NOT_EXISTS_IN_TEAM);
     }
-    
-    
-    
-    
   } catch (err) {
     if (err instanceof Error) {
       // If 'err' is an instance of Error, send the error message
@@ -219,44 +214,45 @@ router.put("/update", async (req: Request, res: Response) => {
 });
 
 // Endpoint to remove player from team
-router.delete("/remove",async (req:Request, res: Response) => {
-    const jerseyId = req.body.jerseyId;
-    const teamId = req.body.teamId;
-    const managerEmail = req.body.userName;
+router.delete("/remove", async (req: Request, res: Response) => {
+  const jerseyId = req.body.jerseyId;
+  const teamId = req.body.teamId;
+  const managerEmail = req.body.userName;
 
-    // Check if any required field is missing
-    if (!jerseyId) {
- 
-      console.log(HttpMsg.BAD_REQUEST);
+  // Check if any required field is missing
+  if (!jerseyId) {
+    console.log(HttpMsg.BAD_REQUEST);
+    res.status(HttpCode.BAD_REQUEST).send({ message: HttpMsg.BAD_REQUEST });
+    return;
+  }
+
+  try {
+    const isRemoved = await playerController.removePlayer(
+      jerseyId,
+      teamId,
+      managerEmail
+    );
+
+    if (isRemoved) {
+      return res.send({ message: "Player removed from team successfully" });
+    } else {
+      throw new Error(HttpMsg.PLAYER_REMOVE_FAILED);
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      // If 'err' is an instance of Error, send the error message
+      res.status(HttpCode.BAD_REQUEST).send({ message: err.message });
+    } else {
+      // If 'err' is of unknown type, send a generic error message
       res.status(HttpCode.BAD_REQUEST).send({ message: HttpMsg.BAD_REQUEST });
-      return;
     }
-  
-    try{
-      const isRemoved = await playerController.removePlayer(jerseyId, teamId, managerEmail);
-
-      if (isRemoved) {
-        return res.send({ message: "Player removed from team successfully" });
-      } else {
-        throw new Error(HttpMsg.PLAYER_REMOVE_FAILED);
-      }
-
-    } catch (err) {
-      if (err instanceof Error) {
-        // If 'err' is an instance of Error, send the error message
-        res.status(HttpCode.BAD_REQUEST).send({ message: err.message });
-      } else {
-        // If 'err' is of unknown type, send a generic error message
-        res.status(HttpCode.BAD_REQUEST).send({ message: HttpMsg.BAD_REQUEST });
-      }
-    }
+  }
 });
 
 // Endpoint to get all the teams of Player
 router.get("/myTeams", async (req, res) => {
   const playerEmail = req.body.userName;
   try {
-
     const teams = await playerController.getTeamsForPlayer(playerEmail);
 
     if (teams.length > 0) {
@@ -265,7 +261,6 @@ router.get("/myTeams", async (req, res) => {
       res.send({ message: "Player is not part of any teams" });
     }
   } catch (err) {
-
     if (err instanceof Error) {
       // If 'err' is an instance of Error, send the error message
       res.status(HttpCode.BAD_REQUEST).send({ message: err.message });
@@ -277,66 +272,75 @@ router.get("/myTeams", async (req, res) => {
 });
 
 // Endpoint to get analytics summary
-router.get("/analytics-summary/:duration",async (req:Request, res: Response) => {
-  if (!req.body.userName) {
-    console.log(HttpMsg.BAD_REQUEST);
-    res.status(HttpCode.BAD_REQUEST).send({ message: HttpMsg.BAD_REQUEST });
-    return;
-  }
-  try {
-    
-    const playerEmail = req.body.userName; // player email is retrieved from the request body
-    console.log(playerEmail);
-
-    // Check if player exists
-    const playerExists = await playerController.checkPlayerExists(playerEmail);
-
-    if (!playerExists) {
-      throw new Error(HttpMsg.PLAYER_DOES_NOT_EXIST);
+router.get(
+  "/analytics-summary/:duration",
+  async (req: Request, res: Response) => {
+    if (!req.body.userName) {
+      console.log(HttpMsg.BAD_REQUEST);
+      res.status(HttpCode.BAD_REQUEST).send({ message: HttpMsg.BAD_REQUEST });
+      return;
     }
+    try {
+      const playerEmail = req.body.userName; // player email is retrieved from the request body
+      console.log(playerEmail);
 
-    // Assuming 'getAnalyticsSummary' is a function in your playerController
-    // const analyticsSummary = await playerController.getAnalyticsSummary(playerEmail, req.params.duration);
-    const analyticsSummary = await playerController.getAnalyticsSummary(playerEmail, req.params.duration);
-    res.send({ analyticsSummary });
-    
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal Server Error" });
+      // Check if player exists
+      const playerExists = await playerController.checkPlayerExists(
+        playerEmail
+      );
+
+      if (!playerExists) {
+        throw new Error(HttpMsg.PLAYER_DOES_NOT_EXIST);
+      }
+
+      // Assuming 'getAnalyticsSummary' is a function in your playerController
+      // const analyticsSummary = await playerController.getAnalyticsSummary(playerEmail, req.params.duration);
+      const analyticsSummary = await playerController.getAnalyticsSummary(
+        playerEmail,
+        req.params.duration
+      );
+      res.send({ analyticsSummary });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
   }
-});
+);
 
 // Endpoint Accept Invitation
 router.get("/accept-invitation/token/:token", async (req, res) => {
   const token = req.params.token;
-  // const player = await PlayerModel.findOne({ invitationToken: token }); 
-  const playerInTeam = await PlayerTeamModel.findOne({ invitationToken: token }); 
+  // const player = await PlayerModel.findOne({ invitationToken: token });
+  const playerInTeam = await PlayerTeamModel.findOne({
+    invitationToken: token,
+  });
   if (playerInTeam && playerInTeam.isVerified == "pending") {
     // Update player status
     playerInTeam.isVerified = "verified";
     await playerInTeam.save();
     res.send("Invitation accepted successfully!");
-  }
-  else{
+  } else {
     res.status(400).send("Invalid or expired token.");
   }
 });
 
 // Endpoint verify email
-router.get("/verify-email/token/:token", async (req: Request, res: Response) => {
-  const token = req.params.token;
-  const player = await PlayerModel.findOne({ invitationToken: token }); 
-  // const playerInTeam = await PlayerTeamModel.findOne({ invitationToken: token }); 
-  if (player && player.isVerified == "pending") {
-    // Update player status
-    player.isVerified = "verified";
-    await player.save();
-    res.send("Invitation accepted successfully!");
+router.get(
+  "/verify-email/token/:token",
+  async (req: Request, res: Response) => {
+    const token = req.params.token;
+    const player = await PlayerModel.findOne({ invitationToken: token });
+    // const playerInTeam = await PlayerTeamModel.findOne({ invitationToken: token });
+    if (player && player.isVerified == "pending") {
+      // Update player status
+      player.isVerified = "verified";
+      await player.save();
+      res.send("Invitation accepted successfully!");
+    } else {
+      res.status(400).send("Invalid or expired token.");
+    }
   }
-  else{
-    res.status(400).send("Invalid or expired token.");
-  }
-});
+);
 
 // Endpoint accept or denied invitation from user profile
 router.get("/accept-invite/:teamId/:isAccepted", async (req, res) => {
@@ -347,7 +351,10 @@ router.get("/accept-invite/:teamId/:isAccepted", async (req, res) => {
   console.log(playerEmail, teamId, isAccepted, typeof isAccepted);
 
   try {
-    const playerInTeam = await PlayerTeamModel.findOne({ playerEmail: playerEmail, teamId: teamId });
+    const playerInTeam = await PlayerTeamModel.findOne({
+      playerEmail: playerEmail,
+      teamId: teamId,
+    });
     if (playerInTeam) {
       if (isAccepted == "1" && playerInTeam.isVerified == "pending") {
         // Update player status
@@ -360,7 +367,7 @@ router.get("/accept-invite/:teamId/:isAccepted", async (req, res) => {
         playerInTeam.isVerified = "rejected";
         await playerInTeam.save();
         res.send("Invitation denied successfully!");
-      }else{
+      } else {
         res.send("Invitation already accepted or denied!");
       }
     } else {
